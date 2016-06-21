5f6a84ab39954466e5d7bdc372ca68c002088e8c
Tyler van Hensbergen <tvanhens@Tylers-MacBook-Pro.local>
2016-06-21 18:48:14 -0400

    Factor rest out of init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,36 +10,9 @@
 ;;------------------------------------------------------------------------------
 ;; Custom Configuration
 
-(load-file "./.emacs.d/global.el")
 (load-file "./.emacs.d/clojure.el")
 (load-file "./.emacs.d/elisp.el")
-
-;;------------------------------------------------------------------------------
-;; Go Mode
-
-(require 'go-mode-autoloads)
-(add-hook 'go-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook 'gofmt-before-save)
-            (setq tab-width 4)
-            (setq indent-tabs-mode 1)))
-
-(setq company-tooltip-limit 20)
-(setq company-idle-delay .3)
-(setq company-echo-delay 0)
-(setq company-begin-commands '(self-insert-command))
-
-(require 'company-go)
-
-;;------------------------------------------------------------------------------
-;; Markdown
-
-(defun bind-markdown-keys ()
-  (local-set-key (kbd "C-c k") 'gh-md-render-buffer))
-
-(add-hook 'markdown-mode-hook #'bind-markdown-keys)
-
-;;------------------------------------------------------------------------------
-;; Javascript
-
-(setq js-indent-level 2)
+(load-file "./.emacs.d/js.el")
+(load-file "./.emacs.d/global.el")
+(load-file "./.emacs.d/go.el")
+(load-file "./.emacs.d/markdown.el")
