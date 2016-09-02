59175c17a4d86e95c75588536e08fafd49fbbe22
Tyler van Hensbergen <tyler@thinktopic.com>
2016-09-02 11:02:41 -0600

    Add cider quit binding

--- emacs.d/clojure.el
+++ emacs.d/clojure.el
@@ -19,11 +19,13 @@
 
 ;; Utility functions
 
+(defun tvh-clj-bindings ()
+  (local-set-key (kbd "C-c q") 'cider-quit))
+
 (defun tvh-clj-mode-hook ()
   (clj-refactor-mode 1)
   (yas-minor-mode 1) ; for adding require/use/import statements
-  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
-  (cljr-add-keybindings-with-prefix "C-c C-m"))
+  )
 
 ;; Hooks
 
@@ -36,3 +38,5 @@
 (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook 'tvh-clj-mode-hook)
+(add-hook 'clojure-mode-hook 'tvh-clj-bindings)
+
