36b6378d9bfbf17edc6c462f68e200773c349387
Tyler van Hensbergen <tvanhens@gmail.com>
2017-02-05 18:54:06 -0700

    Load local.el when it exists

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -25,4 +25,5 @@
 (load-file "./.emacs.d/window.el")
 (load-file "./.emacs.d/term.el")
 (load-file "./.emacs.d/python.el")
-(load-file "./.emacs.d/local.el")
+(when (file-exists-p "./.emacs.d/local.el")
+  (load-file "./.emacs.d/local.el"))
